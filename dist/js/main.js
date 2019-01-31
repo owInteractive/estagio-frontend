df8425e02de865955041504392d090fadc89beda
Cibele Yumi <contato@cibeleyumi.com.br>
2019-01-31 19:09:10 -0200

    WIP - Create phone mask function

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,14 @@
 window.onscroll = function() {
-  
-  var navbar = document.getElementById("navbar");
+
+  var navbar = $('#navbar');
   if (document.body.scrollTop > 132 || document.documentElement.scrollTop > 132) {
-    navbar.classList.add('scrollBarAnimation');
+    navbar.addClass('scrollBarAnimation');
   } else {
-    navbar.classList.remove('scrollBarAnimation');
+    navbar.removeClass('scrollBarAnimation');
   }
 
 };
+
+$(document).ready(function(){
+  $('#phone').inputmask("(99) 9 9999-9999");
+});
